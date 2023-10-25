b1f4b1911643a9c8eb5682280f00daa213d010cd
Raulenzo <raru69@hotmail.com>
2023-10-25 17:16:52 +0200

    DataInitializer

--- src/main/java/parking/infraRepositories/Data/DataInitializer.java
+++ src/main/java/parking/infraRepositories/Data/DataInitializer.java
@@ -2,11 +2,13 @@ package parking.infraRepositories.Data;
 
 import parking.domain.models.Plazas;
 import parking.domain.models.Rol;
+import parking.domain.models.Usuarios;
 import parking.infraRepositories.PlazasRepository;
 import parking.infraRepositories.RolRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import parking.infraRepositories.UsuariosRepository;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
@@ -16,14 +18,15 @@ public class DataInitializer implements CommandLineRunner {
     @Autowired
     private PlazasRepository plazasRepository;
 
+    @Autowired
+    private UsuariosRepository usuariosRepository;
+
     @Override
     public void run(String... args) throws Exception {
-
         Rol rol = new Rol();
         rol.setId(1); // Cambiado a un número si id es numérico
         rol.setNombreRol("Administrador");
         rolRepository.save(rol);
-
 
         Plazas plazas = new Plazas();
         plazas.setId(1);
@@ -32,5 +35,15 @@ public class DataInitializer implements CommandLineRunner {
         plazas.setDisponible(true);
         plazas.setPropietario("si");
         plazasRepository.save(plazas);
+
+        Usuarios usuarios = new Usuarios();
+        usuarios.setId(1);
+        usuarios.setNombre("Raul");
+        usuarios.setEmail("Manolito@manolito");
+        usuarios.setDni("454545464l");
+        usuarios.setTelefono("55156444");
+        usuarios.setRol("usuario");
+        usuariosRepository.save(usuarios);
     }
 }
+
